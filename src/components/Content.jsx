6b8f2838eb1af33004ad130bc6010aebbd458a53
gustavoweb <contato@gustavoweb.com>
2025-03-24 11:39:15 -0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+            <section>
+                <h2>Aqui ficará o Conteúdo</h2>
+            </section>
+        </>
+    );
+};
+export default Content;
