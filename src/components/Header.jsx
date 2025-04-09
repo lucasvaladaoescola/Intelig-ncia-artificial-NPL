93a392a6379885137b78138599b069417406cce9
Lucas Valadão <lucasvaladaoescola@gmail.com>
2025-04-09 17:04:29 -0300

    enviando projeto para deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,7 @@ function Header(){
             <header className="header">
                 <nav>
                     <ul>
-                        <li><a href="#">Valorant</a></li>
-                        <li><a href="#">Heroi</a></li>
-                        <li><a href="#">Contato</a></li>
+                    
                     </ul>
                 </nav>
             </header>
